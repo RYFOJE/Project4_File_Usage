8aa50412370f02591798be762eac6d7c795657e4
Ryan Jennings <ryfoje2@gmail.com>
2023-04-16 00:32:38 -0400

    Before bevs

--- Project4_File_Usage/fs_functions.cpp
+++ Project4_File_Usage/fs_functions.cpp
@@ -17,7 +17,18 @@ FileType::FileType() : count(0), totalBytes(0) {}
 // Functions
 
 // Function that will calculate how many digits in length an int is
-unsigned int calculate_int_length(unsigned int num) {
+unsigned int calculate_length(unsigned int num) {
+
+	unsigned int length = 0;
+	while (num != 0) {
+		num /= 10;
+		++length;
+	}
+
+	return length;
+}
+
+unsigned int calculate_length(unsigned long long int num) {
 
 	unsigned int length = 0;
 	while (num != 0) {
@@ -47,8 +58,29 @@ unsigned int find_longest_filetype(std::map<std::string, FileType>& fileMap ) {
 	return size;
 }
 
+std::string format_int(unsigned long long int number) {
 
+	std::string tmpStr = std::to_string(number);
+	std::string newStr;
 
+	unsigned int count = 0;
+	const int thousandSeperator = 3;
+
+	for (int i = tmpStr.size() - 1; i >= 0; i--, count++) {
+
+		if (count % thousandSeperator == 0 && count != 0) {
+			newStr.append(",");
+		}
+
+		newStr += tmpStr.at(i);
+
+	}
+
+	std::reverse(newStr.begin(), newStr.end());
+
+	return newStr;
+
+}
 
 void FileType::add_file(unsigned long long int size) {
 	count++;
@@ -65,10 +97,54 @@ unsigned long long int FileType::get_total_bytes() const{
 
 void print_map(std::map<std::string, FileType>& fileMap) {
 
+	const unsigned int TOTAL_PADDING = 5;
 
 	std::map<std::string, FileType>::iterator it = fileMap.begin();
 	unsigned int fileTotal = 0;
 	unsigned long long int totalSize = 0;
+
+	unsigned int paddingExtension = 0;
+	unsigned int paddingCount = 0;
+	unsigned int paddingSize = 0;
+
+	// Iterate through fileMap and find the longest of values
+	for (; it != fileMap.end(); it++) {
+
+		unsigned int sizeDigitCount = format_int(it->second.get_total_bytes()).size();
+		unsigned int countDigitCount = calculate_length(it->second.get_count());
+
+		if (paddingSize < sizeDigitCount) {
+			paddingSize = sizeDigitCount;
+		} 
+
+		if (paddingCount < countDigitCount) {
+			paddingCount = countDigitCount;
+		}
+
+	}
+
+	it = fileMap.begin();
+
+	// Find the size of the largest extension string
+	for (; it != fileMap.end(); it++) {
+
+		if (paddingExtension < it->first.size()) {
+			paddingExtension = it->first.size();
+		}
+
+	}
+
+	paddingExtension += 1;
+	paddingCount += TOTAL_PADDING;
+	paddingSize += TOTAL_PADDING;
+
+	it = fileMap.begin();
+
+	// Print the header
+	std::cout << std::setw(paddingExtension) << "Ext";
+	std::cout << std::setw(paddingCount) << "#";
+	std::cout << std::setw(paddingSize) << "Total";
+	std::cout << std::endl << std::endl;
 
 	// Iterate throughout the map
 	for (; it != fileMap.end(); it++) {
@@ -76,7 +152,9 @@ void print_map(std::map<std::string, FileType>& fileMap) {
 		fileTotal += it->second.get_count();
 		totalSize += it->second.get_total_bytes();
 
-		std::cout << std::setw()
+		std::cout << std::setw(paddingExtension) << it->first;
+		std::cout << std::setw(paddingCount) << it->second.get_count();
+		std::cout << std::setw(paddingSize) << format_int(it->second.get_total_bytes()) << std::endl;
 
 	}
 
